f7af594554e3b5f5a214af17f8fe236fde733c55
szoloa <szoloa@hotmail.com>
2025-01-05 13:39:57 +0800

    修改：     .gitignore 	修改：     main.py 	修改：     test.py

--- test.py
+++ test.py
@@ -4,19 +4,24 @@ bg = ColorClip(size=(1920, 1080), color=(255,255,255))
 
 clip = VideoFileClip("input.mp4")
 
-clip = clip.resized((1920*0.8, 1080*0.8))
+clip = clip.resized(0.45)
 
-txt_clip = TextClip(text="My Holidays 2013",font='Hack-Regular',font_size=160, color='black')
+clip = clip.with_position((640, 60))
+
+txt_clip = TextClip(text="The girl \nI want",font='simsun',font_size=84, color='black')
+txt_clip = txt_clip.with_position((120, 1080/4-84/2))
+
+txt_clip_2 = TextClip(text="What I \nlooks like",font='simsun',font_size=84, color='black')
+txt_clip_2 = txt_clip_2.with_position((120, 1080*3/4-84/2))
 
 txt_clip = txt_clip.with_end(clip.end)
+txt_clip_2 = txt_clip_2.with_end(clip.end)
 
 bg = bg.with_end(clip.end)
 
 # Overlay the text clip on the first video clip
-video = CompositeVideoClip([bg, clip, txt_clip])
+video = CompositeVideoClip([bg, clip, txt_clip, txt_clip_2])
 
-video.fps=25
+video.preview(audio=False)
 
-# video.preview()
-
-video.write_videofile("output.mp4")
+# video.write_videofile("output.mp4")
